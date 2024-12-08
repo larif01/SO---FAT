5569eedc85f8116056fa299682a8dcf8cb2391df
larif01 <larissa.fantin01@gmail.com>
2024-12-08 14:32:56 -0300

    Update output.c

--- output.c
+++ output.c
@@ -2,56 +2,53 @@
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-static struct pretty_int { int num; char* suff; } pretty_print(int value)
+static struct pretty_int pretty_print(int value)
 {
+    static char* B   = "B";
+    static char* KiB = "KiB";
+    static char* MiB = "MiB";
 
-	static char* B   = "B";
-	static char* KiB = "KiB";
-	static char* MiB = "MiB";
+    struct pretty_int res =
+    {
+        .num  = value,
+        .suff = B
+    };
 
-	struct pretty_int res =
-	{
-		.num  = value,
-		.suff = B
-	};
+    if (res.num == 0) return res;
 
-	if (res.num == 0) return res;
+    if (res.num >= 1024)
+    {
+        res.num  /= 1024;
+        res.suff = KiB;
+    }
 
-	if (res.num >= 1024)
-	{
-		res.num  /= 1024;
-		res.suff = KiB;
-	}
+    if (res.num >= 1024)
+    {
+        res.num  /= 1024;
+        res.suff = MiB;
+    }
 
-	if (res.num >= 1024)
-	{
-		res.num  /= 1024;
-		res.suff = MiB;
-	}
-
-	return res;
+    return res;
 }
 
 void show_files(struct fat_dir *dirs)
 {
+    struct fat_dir *cur;
 
-	struct fat_dir *cur;
-
-	fprintf(stdout, "ATTR  NAME    FMT    SIZE\n-------------------------\n");
+    fprintf(stdout, "ATTR  NAME    FMT    SIZE\n-------------------------\n");
 
     while ((cur = dirs++) != NULL)
-	{
-
-		if (cur->name[0] == 0)
-			break;
+    {
+        if (cur->name[0] == 0)
+            break;
 
         else if ((cur->name[0] == DIR_FREE_ENTRY) || (cur->attr == DIR_FREE_ENTRY))
             continue;
 
-		else if (cur->attr == 0xf)
-			continue;
+        else if (cur->attr == DIR_ATTR_LFN) // Long File Name attribute
+            continue;
 
-		struct pretty_int num = pretty_print(cur->file_size);
+        struct pretty_int num = pretty_print(cur->file_size);
 
         fprintf(stdout, "0x%-.*x  %.*s  %4i %s\n", 2, cur->attr, FAT16STR_SIZE, cur->name, num.num, num.suff);
     }
@@ -61,7 +58,6 @@ void show_files(struct fat_dir *dirs)
 
 void verbose(struct fat_bpb *bios_pb)
 {
-
     fprintf(stdout, "Bios parameter block:\n");
     fprintf(stdout, "Jump instruction: ");
 
@@ -70,16 +66,14 @@ void verbose(struct fat_bpb *bios_pb)
 
     fprintf(stdout, "\n");
 
-    fprintf(stdout, "OEM ID: %s\n", bios_pb->oem_id);
+    fprintf(stdout, "OEM ID: %.8s\n", bios_pb->oem_id);
     fprintf(stdout, "Bytes per sector: %d\n", bios_pb->bytes_p_sect);
     fprintf(stdout, "Sector per cluster: %d\n", bios_pb->sector_p_clust);
     fprintf(stdout, "Reserved sector: %d\n", bios_pb->reserved_sect);
     fprintf(stdout, "Number of FAT copies: %d\n", bios_pb->n_fat);
-    fprintf(stdout, "Number of possible entries: %d\n", bios_pb->possible_rentries);
-    fprintf(stdout, "Small number of sectors: %d\n", bios_pb->snumber_sect);
+    fprintf(stdout, "Root cluster: %d\n", bios_pb->root_cluster);
     fprintf(stdout, "Media descriptor: %hhX\n", bios_pb->media_desc);
-    fprintf(stdout, "Sector per fat: %d\n", bios_pb->sect_per_fat);
-
+    fprintf(stdout, "Sector per FAT: %d\n", bios_pb->fat_size32);
     fprintf(stdout, "Sector per track: %d\n", bios_pb->sect_per_track);
     fprintf(stdout, "Number of heads: %d\n", bios_pb->number_of_heads);
 
@@ -87,5 +81,5 @@ void verbose(struct fat_bpb *bios_pb)
     fprintf(stdout, "Root Address: 0x%x\n", bpb_froot_addr(bios_pb));
     fprintf(stdout, "Data Address: 0x%x\n", bpb_fdata_addr(bios_pb));
 
-	return;
+    return;
 }
